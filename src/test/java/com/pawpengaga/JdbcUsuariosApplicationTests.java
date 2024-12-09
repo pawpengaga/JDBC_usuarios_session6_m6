ba3e921a9fdfe7a89a02d76f9462d6905572814d
Erick Rivera <yomellamoerick.771@gmail.com>
2024-12-09 19:41:57 -0300

    primer commit y conexion

---
+++ src/test/java/com/pawpengaga/JdbcUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawpengaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
